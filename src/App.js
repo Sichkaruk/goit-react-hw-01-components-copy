cb69b4e4876ed493b9a0b24fe743c91be4b5a34d
Bohdan Sichkaruk <82411127+Sichkaruk@users.noreply.github.com>
2022-05-26 18:20:50 +0300

    made a profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import user from "./data/user.json";
+import Profile from "./components/profile/Profile";
+
 function App() {
-  return <div className="App">Hello Sweetie!</div>;
+  return (
+    <div className="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,31 @@
+import s from "./Profile.module.css";
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.username}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li className={s.statsItem}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
